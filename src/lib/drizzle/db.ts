91a3e8b9fd041e57731a306d8db56c1e1d3b8b9e
Arridha Amrad <arridhaamrad@gmail.com>
2025-04-07 12:10:23 +0700

    trash

--- src/lib/drizzle/db.ts
+++ src/lib/drizzle/db.ts
@@ -1,14 +1,24 @@
 import { Pool } from "@neondatabase/serverless";
 import { ExtractTablesWithRelations } from "drizzle-orm";
-import { drizzle, NeonQueryResultHKT } from "drizzle-orm/neon-serverless";
+import {
+  drizzle as neonDrizzle,
+  NeonQueryResultHKT,
+} from "drizzle-orm/neon-serverless";
 import { PgTransaction } from "drizzle-orm/pg-core";
 import * as schema from "./schema";
 
-const pool = new Pool({ connectionString: process.env.DB_URL });
-
-export const db = drizzle(pool, {
-  schema,
+const pool = new Pool({
+  connectionString: process.env.DB_URL,
 });
+
+import { drizzle as pgDrizzle } from "drizzle-orm/node-postgres";
+
+export const db =
+  process.env.NODE_ENV === "development"
+    ? pgDrizzle(process.env.DEV_DB_URL!)
+    : neonDrizzle(pool, {
+        schema,
+      });
 
 export type DrizzleDatabase = typeof db;
 export type DrizzleTransactionDatabase = PgTransaction<
